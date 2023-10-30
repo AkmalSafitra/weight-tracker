097a931ae7d70c015590467e689b810cedbe008c
akmalsafitra <akmal.safitra@gmail.com>
2023-10-30 07:07:50 +0700

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "weighttracker2e201d66": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ android/app/src/main/kotlin/com/example/weight_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gn.weight_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
